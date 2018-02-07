85e9190bf415c7b6126299645a23b932a7104414
ybon3 <lee.ybon@gmail.com>
2018-02-07 14:03:08 +0800

    修改 dicom 變更識別欄位的規則

--- src/main/java/com/dtc/strees/fidxc/ArchiveTest.java
+++ src/main/java/com/dtc/strees/fidxc/ArchiveTest.java
@@ -1,13 +1,15 @@
 package com.dtc.strees.fidxc;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
 import org.dcm4che3.data.Attributes;
 import org.dcm4che3.data.Tag;
 import org.dcm4che3.data.VR;
+
+import com.dtc.strees.util.AccessionNumberGenerator;
+import com.dtc.strees.util.StudyIdGenerator;
 
 public class ArchiveTest extends AttributeModifier {
 
@@ -27,12 +29,17 @@ public class ArchiveTest extends AttributeModifier {
 
 	@Override
 	protected Attributes modifyAttrs(Attributes attrs) {
-		String sopInstanceUid = UUID.randomUUID().toString();
-		attrs.setString(Tag.MediaStorageSOPInstanceUID, VR.UI, sopInstanceUid);
+		String studyInstanceUID = StudyIdGenerator.next();
+		attrs.setString(Tag.StudyInstanceUID, VR.UI, studyInstanceUID); //lv1
+
+		String seriesInstanceUID = studyInstanceUID + "." + 1;
+		attrs.setString(Tag.SeriesInstanceUID, VR.UI, seriesInstanceUID); //lv1.lv2
+
+		String sopInstanceUid = seriesInstanceUID + "." + 2;
+		attrs.setString(Tag.MediaStorageSOPInstanceUID, VR.UI, sopInstanceUid); //lv1.lv2.lv3
 		attrs.setString(Tag.SOPInstanceUID, VR.UI, sopInstanceUid);
-		attrs.setString(Tag.StudyInstanceUID, VR.UI, UUID.randomUUID().toString());
-		attrs.setString(Tag.SeriesInstanceUID, VR.UI, UUID.randomUUID().toString());
-		attrs.setString(Tag.AccessionNumber, VR.UI, UUID.randomUUID().toString());
+
+		attrs.setString(Tag.AccessionNumber, VR.UI, AccessionNumberGenerator.next()); //15碼以內, 英數
 		return attrs;
 	}
 }
